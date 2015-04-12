6f7e94eaeb91e11632f7c39bb34c00c85c737a03
Andre Venter <andre.n.venter@gmail.com>
2015-04-12 17:27:22 +0200

    cleanup

--- app/editController.js
+++ app/editController.js
@@ -5,10 +5,8 @@ app.controller('EditController', [
     'LinksAPI',
 
     function ($scope, $stateParams, $state, LinksAPI) {
-        
-        $scope.link = ($.grep(LinksAPI.links, function(e){ return e.id == $stateParams.id; }))[0];
 
-        console.log($scope.link);
+        $scope.link = ($.grep(LinksAPI.links, function(e){ return e.id == $stateParams.id; }))[0];
 
         $scope.editLink = function (link) {
             LinksAPI.update(link).then(function () {
