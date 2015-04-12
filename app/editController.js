f40d8eb9898a02590417d9bccba4ad6244ad4c54
Andre Venter <andre.n.venter@gmail.com>
2015-04-12 17:08:42 +0200

    add edit the selected object not array item

--- app/editController.js
+++ app/editController.js
@@ -5,8 +5,10 @@ app.controller('EditController', [
     'LinksAPI',
 
     function ($scope, $stateParams, $state, LinksAPI) {
+        
+        $scope.link = ($.grep(LinksAPI.links, function(e){ return e.id == $stateParams.id; }))[0];
 
-        $scope.link = LinksAPI.links[$stateParams.id];
+        console.log($scope.link);
 
         $scope.editLink = function (link) {
             LinksAPI.update(link).then(function () {
